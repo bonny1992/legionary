18bd7b1039d1321e9c49dadd37e1c580325fd74c
bonny1992 <lh_firepower@hotmail.com>
2016-09-01 01:39:32 +0200

    Fixed a lot of things

--- app.js
+++ app.js
@@ -242,3 +242,17 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
 		}
 	});
 });
+
+
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
