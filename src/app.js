6acb61035f60cb91e6a1935ef0d16ded3cc5cd03
bonny1992 <lh_firepower@hotmail.com>
2016-09-01 18:45:01 +0200

    Fixed !addurl command

--- src/app.js
+++ src/app.js
@@ -217,10 +217,10 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
 																e.message.channel.sendMessage(`${e.message.author.mention}: Non è stato possibile aggiungere il torrent!`);
 															}
 															else {
-																console.log(result);
 																const magnet = require('magnet-uri');
 																const parsed = magnet.decode(message[1]);
 																const log_string = `${e.message.author.username} ha aggiunto il torrent ${parsed.dn}`;
+																fs.appendFileSync('public/logs.html',log_string);
 																console.log(log_string);
 																const big_message = `${e.message.author.mention}: È stato aggiunto il torrent con magnet\n\n\`\`\`${message[1]}\`\`\`\ncon titolo **${parsed.dn}** con successo!`;
 																e.message.channel.sendMessage(big_message);
